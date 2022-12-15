221f1f9ca975a9a8078b568c9752149585957f33
Armando <lx03136@innovaccion.mx>
2022-12-14 18:00:26 -0600

    adding mysql and xlsx libraries

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const xlsx = require('xlsx');
 
 const excel = require('exceljs');
 //CREATE A CONNECTION TO DB 
@@ -19,7 +20,7 @@ con.connect((err) => {
 
 /*var qry= "SELECT C.c_cliente_nombre, F.c_flotilla_nombre U.c_unidad_nombre  FROM dat_Unidad U, dat_Cliente C, dat_Flotilla F, rel_UnidadFlotilla UF WHERE U.n_estatus_id = 1 AND C.n_estatus_id = 1 AND F.n_flotilla_id AND AND UF.n_estatus_id U.b_unidad_altotrack = 1 AND C.n_cliente_id = U.n_cliente_id AND C.n_cliente_id = F.n_cliente_id AND UF.n_unidad_id = U.n_unidad_id  AND UF.n_flotilla_id = F.n_flotilla_id";*/
 
-var qry= 'SELECT DISTINCT C.c_cliente_nombre, F.c_flotilla_nombre, U.c_unidad_nombre, CONCAT("Altotrack") AS "WS" FROM dat_Unidad U, dat_Cliente C, dat_Flotilla F, rel_UnidadFlotilla UF WHERE U.n_estatus_id = 1  AND C.n_estatus_id = 1  AND F.n_estatus_id =1 AND UF.n_estatus_id = 1 AND b_unidad_altotrack = 1  AND C.n_cliente_id = U.n_cliente_id  AND C.n_cliente_id = F.n_cliente_id AND UF.n_unidad_id = U.n_unidad_id  AND UF.n_flotilla_id = F.n_flotilla_id';
+var qry= 'SELECT DISTINCT C.c_cliente_nombre, F.c_flotilla_nombre, U.c_unidad_nombre, CONCAT("Altotrack") AS "WS" FROM dat_Unidad U, dat_Cliente C, dat_Flotilla F, rel_UnidadFlotilla UF WHERE U.n_estatus_id = 1  AND C.n_estatus_id = 1  AND F.n_estatus_id =1 AND UF.n_estatus_id = 1 AND b_unidad_altotrack = 1  AND C.n_cliente_id = U.n_cliente_id  AND C.n_cliente_id = F.n_cliente_id AND UF.n_unidad_id = U.n_unidad_id  AND UF.n_flotilla_id = F.n_flotilla_id UNION ALL (SELECT DISTINCT C.c_cliente_nombre, F.c_flotilla_nombre, U.c_unidad_nombre, CONCAT("Recurso confiable") AS "WS" FROM dat_Unidad U, dat_Cliente C, dat_Flotilla F, rel_UnidadFlotilla UF WHERE U.n_estatus_id = 1  AND C.n_estatus_id = 1  AND F.n_estatus_id =1 AND UF.n_estatus_id = 1 AND b_unidad_rconfiable = 1  AND C.n_cliente_id = U.n_cliente_id  AND C.n_cliente_id = F.n_cliente_id AND UF.n_unidad_id = U.n_unidad_id  AND UF.n_flotilla_id = F.n_flotilla_id)';
 
 /*var qry = "SELECT DISTINCT C.c_cliente_nombre, U.c_unidad_nombre FROM dat_Unidad U, dat_Cliente C WHERE C.n_cliente_id = U.n_cliente_id AND U.n_estatus_id = 1 AND U.b_unidad_dhl = 1";*/
 con.query(qry, (err, result) => {
